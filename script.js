771a95ac48a86486c54804e3ca4bfe67fbcdb298
Thomas Brakutt <thomas.brakutt@gmail.com>
2024-10-23 20:55:13 +0200

    start working on Kata

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+https://github.com/coding-bootcamps-eu/web-apps-foundation/blob/main/web-app-basics/02-password-toggle.md
+
+Password Toggle
+
+Implement a web app that will toggle the visibility of a password like
+this example (https://coding-katas.netlify.app/password-toggle/).
+
+Requirments
+ Add a input that holds a fictive password
+ Add a button that will toggle the readability of the password in the input
+ Change the text of the button to "Show Password" if the password is hidden
+ Change the text of the button to "Hide Password" if the password is visible
+
+Hints
+<!-- Text input -->
+<input type="text">
+
+<!-- Password input -->
+<input type="password">
+*/
